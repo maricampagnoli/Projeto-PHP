4ba47a3a5198ffc1d1c93b52b346fdfe9c04763b
mcampagnoli1 <85307849+mcampagnoli1@users.noreply.github.com>
2021-11-11 08:32:51 -0300

    Create conexao.php

---
+++ src/model/conexao.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$usuario = 'root';
+$senha = '';
+$db = 'lanchonete';
+
+$conexao = new mysqli($host, $usuario, $senha, $db);
+if($conexao -> connect_errno){
+    die ("Erro: $conexao->connect_errno");
+}
+else{
+    echo "Conexao ok";
+}
+?>
